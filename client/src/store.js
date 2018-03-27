33091c1811f2c72fcc4af2f43fd9540b3ac28939
Ray Suyoto <ray.suy@icloud.com>
2018-03-27 09:57:41 -0600

    add middleware and redux

---
+++ client/src/store.js
@@ -0,0 +1,22 @@
+import { createStore, compose, applyMiddleware } from 'redux';
+import apiMiddleware from 'redux-devise-axios';
+import axios from 'axios';
+import rootReducer from './reducers/index';
+
+const options = { axios }; 
+
+const enhancers = compose (
+  applyMiddleware(thunk, apiMiddleware(options)),
+  window.devToolsExtension ? window.devToolsExtension() : f => f
+)
+
+const store = createStore(rootReducer, [], enhancers);
+
+if(module.hot) {
+  module.hot.accept('./reducers/', () => {
+    const nextRootReducer = require('./reducers/index').default
+    store.replaceReducer(nextRootReducer)
+  })
+}
+
+export default store ;
