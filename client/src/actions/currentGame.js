00174e1acb5a6b035e4d017f5f5515ac26c31017
Ray Suyoto <ray.suy@icloud.com>
2018-03-27 10:35:50 -0600

    reducers

--- client/src/actions/currentGame.js
+++ client/src/actions/currentGame.js
@@ -1,2 +1,30 @@
 export const ROLL_DICE = 'ROLL_DICE';
 export const TOGGLE_KEPT = 'TOGGLE_KEPT';
+
+export const rollDice = () => {
+  return (dispatch, getState) => {
+    const { keep, dice } = getState().currentGame
+
+    const newDice = dice.map( (value, index) => {
+      if (keep.includes(index))
+        return value
+      return Math.floor(Math.random() * 6) + 1
+    })
+
+    dispatch({ type: ROLL_DICE, dice: newDice })
+  }
+}
+
+export const toggleKept = (index) => {
+  return (dispatch, getState) => {
+    const { keep } = getState().currentGame
+    let updated;
+
+    if (keep.includes(index))
+      updated = keep.filter( k => k !== index )
+    else
+      updated = [...keep, index]
+
+    dispatch({ type: TOGGLE_KEPT, keep: updated })
+  }
+}
